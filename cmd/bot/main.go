bf80033d3b9f60f3a8c10ff70d8cbadee94ece97
Mariia Kalina <kalina@lpmotor.ru>
2022-09-26 23:54:23 +0500

    [GOHW-1]: init repo

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,3 +1,25 @@
 package main
 
-func main() {}
+import (
+	"log"
+
+	"gitlab.ozon.dev/mary.kalina/telegram-bot/internal/clients/tg"
+	"gitlab.ozon.dev/mary.kalina/telegram-bot/internal/config"
+	"gitlab.ozon.dev/mary.kalina/telegram-bot/internal/model/messages"
+)
+
+func main() {
+	cfg, err := config.New()
+	if err != nil {
+		log.Fatal("config init failed:", err)
+	}
+
+	tgClient, err := tg.New(cfg)
+	if err != nil {
+		log.Fatal("tg client init failed:", err)
+	}
+
+	msgModel := messages.New(tgClient)
+
+	tgClient.ListenUpdates(msgModel)
+}
---
+++ go.mod
@@ -0,0 +1,17 @@
+module gitlab.ozon.dev/mary.kalina/telegram-bot
+
+go 1.19
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/golang/mock v1.6.0
+	github.com/google/go-cmp v0.5.9
+	github.com/pkg/errors v0.9.1
+	github.com/stretchr/testify v1.8.0
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+)
